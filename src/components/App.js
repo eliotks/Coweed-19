71fa2103adc29487ac241f343098e1c7e47ad064
eliotks <62217010+eliotks@users.noreply.github.com>
2020-03-15 20:53:20 +0100

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+import Wtf from "./Wtf";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <header className="App-header">Coweed-19</header>
+        <h1>____________________________________________________</h1>
+        <p>Under kommer en egen komponent</p>
+      </div>
+      <Wtf/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Wtf.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Wtf() {
+    return (
+        <div className="Wtf">
+            <h1>COWEED</h1>
+            <h2>COWEED</h2>
+            <h3>COWEED</h3>
+            <h4>COWEED</h4>
+            <h5>COWEED</h5>
+            <h6>COWEED</h6>
+        </div>
+    );
+}
+
+export default Wtf;
