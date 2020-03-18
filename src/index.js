ff75ecb30f96c763f8021fb262c470ccfceecad0
eliotks <eliot.strobel@gmail.com>
2020-03-18 03:21:41 +0100

    added chess hihi

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './chess/chess_index.css';
+import Game from "./chess/components/Game";
 import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
